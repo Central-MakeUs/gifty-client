b9fafc002863c538cb65c4f6e31c9b653c2e37df
kongnayeon <seoultownroad@naver.com>
2023-06-27 17:11:47 +0900

    feat: 상태 관리 라이브러리 설치, theme provider 세팅

---
+++ src/styles/theme/palette.ts
@@ -0,0 +1,43 @@
+export const palette = {
+  primary_nomal: '#0080ff',
+  primary_strong: '#0073e5',
+  primary_alternative: '#66b2ff',
+  primary_assistive: '#d6ebff',
+  primary_pressed: 'rgba(0, 115, 229, 0.07)',
+
+  secondary_nomal: '#00aaff',
+  secondary_strong: '#0099e5',
+  secondary_alternative: '#66ccff',
+  secondary_assistive: '#d6f1ff',
+  secondary_pressed: 'rgba(0, 153, 229, 0.07)',
+
+  fill_pressed: 'rgba(241, 242, 243, 0.5)',
+
+  white: '#ffffff',
+  black: '#000000',
+  gradation: 'linear-gradient(180deg, #00AAFF 0%, #0080FF 100%)',
+
+  status_alert: '#f0425f',
+  status_caution: '#ffd91a',
+  status_positive: '#19e690',
+  status_pressed: '#e8304f',
+
+  text_strong: '#000000',
+  text_nomal: '#181a1b',
+  text_alternative: '#7a8085',
+  text_assistive: '#bdbfc2',
+  text_disable: '#d7d9da',
+
+  background_primary: '#f9fafb',
+  background_secondary: '#ffffff',
+  background_teritary: '#f0f2f4',
+
+  line_nomal: '#e4e5e7',
+  line_alternative: '#f2f2f3',
+  line_disable: '#d1d1d1',
+
+  fill_nomal: '#f2f2f3',
+  fill_strong: '#d7d9da',
+  fill_alternative: '#f1f2f3',
+  fill_disable: '#d1d1d1',
+};
