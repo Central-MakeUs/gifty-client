7f64e6333a1acbed4b0f4f676e4dd7007b6d6ba4
kongnayeon <seoultownroad@naver.com>
2023-08-05 15:06:16 +0900

    feat: 로그인 관련 설정

---
+++ src/screens/login/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Button} from 'react-native';
+
+export const LoginScreen = () => {
+  return (
+    <SafeAreaView>
+      <Button title="카카오 로그인" />
+    </SafeAreaView>
+  );
+};
