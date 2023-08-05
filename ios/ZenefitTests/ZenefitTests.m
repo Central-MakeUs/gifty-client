57a3ac5e57afad1ba76189b575c5adf3c1a9cc22
kongnayeon <seoultownroad@naver.com>
2023-08-05 14:29:42 +0900

    feat: 패키지명 변경

--- ios/ZenefitTests/ZenefitTests.m
+++ ios/ZenefitTests/ZenefitTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface GiftyTests : XCTestCase
+@interface ZenefitTests : XCTestCase
 
 @end
 
-@implementation GiftyTests
+@implementation ZenefitTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
